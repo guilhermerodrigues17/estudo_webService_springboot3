c0bcfdcde8c89f039dfa3dbe465c4cc7f898aaf8
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-11-29 19:38:13 -0300

    Iniciando projeto! Dependências instaladas

---
+++ src/main/java/com/estudoProjectWebService/project_webService_spring/ProjectWebServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.estudoProjectWebService.project_webService_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebServiceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWebServiceSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudoProjectWebService/project_webService_spring/ProjectWebServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudoProjectWebService.project_webService_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebServiceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
