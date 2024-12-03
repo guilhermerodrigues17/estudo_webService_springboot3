f4b3acf407eb6e47abfe521d1ec70a853cac93b0
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-12-03 19:38:18 -0300

    feat(services): Implementada classe UserService

---
+++ src/main/java/com/estudoProjectWebService/project_webService_spring/service/UserService.java
@@ -0,0 +1,19 @@
+package com.estudoProjectWebService.project_webService_spring.service;
+
+import com.estudoProjectWebService.project_webService_spring.entities.User;
+import com.estudoProjectWebService.project_webService_spring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
