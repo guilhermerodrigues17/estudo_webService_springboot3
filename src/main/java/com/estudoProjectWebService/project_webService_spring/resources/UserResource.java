20568205c61c3285baba731031589b0425fba71b
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-12-03 19:40:38 -0300

    refactor(resources): Alterado método 'findAll' para atender ao UserService

--- src/main/java/com/estudoProjectWebService/project_webService_spring/resources/UserResource.java
+++ src/main/java/com/estudoProjectWebService/project_webService_spring/resources/UserResource.java
@@ -1,18 +1,25 @@
 package com.estudoProjectWebService.project_webService_spring.resources;
 
 import com.estudoProjectWebService.project_webService_spring.entities.User;
+import com.estudoProjectWebService.project_webService_spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> userList = service.findAll();
+        return ResponseEntity.ok().body(userList);
     }
 }
