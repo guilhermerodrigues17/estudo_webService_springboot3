fa16ad53b1633259cba5f3d6a71e04e77c42540d
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-12-05 18:11:37 -0300

    feat(resources): Implementada classe 'CategoryResource'

---
+++ src/main/java/com/estudoProjectWebService/project_webService_spring/resources/CategoryResource.java
@@ -0,0 +1,32 @@
+package com.estudoProjectWebService.project_webService_spring.resources;
+
+import com.estudoProjectWebService.project_webService_spring.entities.Category;
+import com.estudoProjectWebService.project_webService_spring.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> categoryList = service.findAll();
+        return ResponseEntity.ok().body(categoryList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+        Category obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
