73b3d22ee98174cec08ee9d2f09ac063aa026683
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-12-06 16:59:45 -0300

    feat(entities): Implementada relação um-para-muitos entre 'Product' e 'OrderItem'

--- src/main/java/com/estudoProjectWebService/project_webService_spring/entities/Product.java
+++ src/main/java/com/estudoProjectWebService/project_webService_spring/entities/Product.java
@@ -1,5 +1,6 @@
 package com.estudoProjectWebService.project_webService_spring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -30,6 +31,9 @@ public class Product implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -84,6 +88,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> orderSet = new HashSet<>();
+        for (OrderItem obj : items) {
+            orderSet.add(obj.getOrder());
+        }
+        return orderSet;
     }
 
     @Override
