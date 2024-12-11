9ce1a28205942309a0bf89c36cc04ced9a53bb4a
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-12-11 15:51:20 -0300

    feat(resources/Exceptions): Implementado handling para 'DatabaseException'

--- src/main/java/com/estudoProjectWebService/project_webService_spring/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/estudoProjectWebService/project_webService_spring/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.estudoProjectWebService.project_webService_spring.resources.exceptions;
 
+import com.estudoProjectWebService.project_webService_spring.service.exceptions.DatabaseException;
 import com.estudoProjectWebService.project_webService_spring.service.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,14 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database violation exception";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
