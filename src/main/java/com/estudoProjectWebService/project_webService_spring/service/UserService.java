4c97f9dc1a3a554731876ac2fcbeedf62baf8b2f
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-12-11 15:54:46 -0300

    style(services/UserService): Atualizada lista de imports da classe

--- src/main/java/com/estudoProjectWebService/project_webService_spring/service/UserService.java
+++ src/main/java/com/estudoProjectWebService/project_webService_spring/service/UserService.java
@@ -6,7 +6,6 @@ import com.estudoProjectWebService.project_webService_spring.service.exceptions.
 import com.estudoProjectWebService.project_webService_spring.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
