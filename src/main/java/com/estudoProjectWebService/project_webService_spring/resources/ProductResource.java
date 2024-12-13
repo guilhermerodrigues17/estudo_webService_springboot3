31efaff961da7f25c985561b997efe45578ae496
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-12-13 12:29:03 -0300

    feat(resources/Product): Implementado e mapeado endpoint para operação 'insert' em ProductResource

--- src/main/java/com/estudoProjectWebService/project_webService_spring/resources/ProductResource.java
+++ src/main/java/com/estudoProjectWebService/project_webService_spring/resources/ProductResource.java
@@ -4,11 +4,10 @@ import com.estudoProjectWebService.project_webService_spring.entities.Product;
 import com.estudoProjectWebService.project_webService_spring.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +27,12 @@ public class ProductResource {
     public ResponseEntity<Product> findById(@PathVariable Long id) {
         Product obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> insert(@RequestBody Product productObj) {
+        productObj = service.insert(productObj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(productObj.getId()).toUri();
+        return ResponseEntity.created(uri).body(productObj);
     }
 }
